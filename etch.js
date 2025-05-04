90dcb5cdd1ef8229ff8e9c642850f8d28cd25b28
nate <199334109+nateTheGreat25@users.noreply.github.com>
2025-05-04 13:36:48 -0500

    can make the divs

--- etch.js
+++ etch.js
@@ -5,8 +5,22 @@ function makeRow () {
     const rowDiv = document.createElement("div");
     rowDiv.classList.add("rowDiv");
     rowDiv.setAttribute("id", "rowDiv");
-    rowDiv.textContent = "Does this work?"
+    
     containerDiv.appendChild(rowDiv);
 }
 
+
+
+function makeSquare (squares) {
+    const rowDiv = document.querySelector("#rowDiv");
+    for (let i = 0; i < squares; i++) {
+        const drawSquare = document.createElement("div");
+        drawSquare.classList.add("square");
+        drawSquare.setAttribute("id", "square");    
+        rowDiv.appendChild(drawSquare);
+        //console.log("test");
+    }
+}
+
 makeRow();
+makeSquare(16);
