77f38e12eb951705dd809a508deb409b8acc0780
nate <199334109+nateTheGreat25@users.noreply.github.com>
2025-05-04 21:49:26 -0500

    able to draw squares

--- etch.js
+++ etch.js
@@ -1,28 +1,30 @@
 const containerDiv = document.querySelector("#container");
 
 // makes div row to put div sqaures in
-function makeRow () {
+function makeRow (i) {
     const rowDiv = document.createElement("div");
     rowDiv.classList.add("rowDiv");
-    rowDiv.setAttribute("id", "rowDiv");
+    rowDiv.setAttribute("id", "rowDiv"+i);
     
     containerDiv.appendChild(rowDiv);
 }
 
 
 
-function makeSquare (squares) {
-    const rowDiv = document.querySelector("#rowDiv");
+function makeSquare (squares, i) {
+    const rowDiv = document.querySelector("#rowDiv"+ i);
     for (let i = 0; i < squares; i++) {
         const drawSquare = document.createElement("div");
         drawSquare.classList.add("square");
-        drawSquare.setAttribute("id", "square");    
+        drawSquare.setAttribute("id", "square"+ i);    
         rowDiv.appendChild(drawSquare);
         //console.log("test");
     }
 }
 
 
+for (let i = 0; i < 16; i++){
+makeRow(i);
 
-makeRow();
-makeSquare(16);
+makeSquare(16, i);
+}
